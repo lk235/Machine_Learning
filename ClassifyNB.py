8a539670cc18a983a4eeb71dd0214cc7bdee431d
lk235 <cool.lk235@icloud.com>
2017-11-30 08:53:44 +0100

    11.30 day

--- ClassifyNB.py
+++ ClassifyNB.py
@@ -4,7 +4,11 @@ def classify(features_train, labels_train):
     clf.fit(features_train,labels_train)
     return clf
 
-
+def classify_svm(features_train, labels_train):
+    from sklearn.svm import SVC
+    clf = SVC(kernel="linear")
+    clf.fit(features_train, labels_train)
+    return clf
 ### import the sklearn module for GaussianNB
 ### create classifier
 ### fit the classifier on the training features and labels
